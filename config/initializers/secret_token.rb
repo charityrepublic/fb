334bbf76cf79193466fc5206755a78835516e0d1
Michael Chang <thenewme91@gmail.com>
2012-11-23 20:06:27 -0500

    Scaffold basic rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charityrepublic::Application.config.secret_token = '4c13f0182098f30c556202c1b2b695229087cf8a3255b34b1e60a2c55078136d12fb721a3c96eeecb31076c1cab6baaf4f7b7a37e171ba0dad9335b0cc34747c'
